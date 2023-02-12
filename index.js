a9872923395b1c489d05634e193e21189adcef7a
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-02-13 00:06:35 +0100

    return a response from module with expressjs

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const myModule = require("./module/module");
 
-console.log("Hello, World!");
-myModule.greet();
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send(myModule.greet());
+});
+
+app.listen(3001, function() {
+  console.log("Example app listening on port 3000!");
+});
--- module/module.js
+++ module/module.js
@@ -1,3 +1,3 @@
 exports.greet = function() {
-    console.log("Hello from module!");
+    return ("Hello from module!");
 };
