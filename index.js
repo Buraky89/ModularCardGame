fe12313364f3ff6005fe2da9aeb89c3a8909fa22
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-02-18 19:19:57 +0100

    make clients controllable through ui

--- index.js
+++ index.js
@@ -9,17 +9,80 @@ const app = express();
 const client1 = new Client(1, "Client 1");
 const client2 = new Client(2, "Client 2");
 
+app.use(express.json());
+
 app.get("/", function(req, res) {
   //res.send(myModule.greet());
-
-  
-
   runGame([client1, client2]);
-
+  
   res.send("ok");
-
 });
 
-app.listen(3001, function() {
+app.get("/client1", (req, res) => {
+  const deck = client1.getDeck();
+  let deckButtons = "";
+  for (let i = 0; i < deck.length; i++) {
+    deckButtons += `<button onclick="submitCard(${i})">${deck[i].cardType} ${deck[i].score}</button>`;
+  }
+  res.send(`
+    <html>
+      <body>
+        <p>${client1.name}'s current deck:</p>
+        ${deckButtons}
+        <script>
+          function submitCard(cardIndex) {
+            fetch('/client1/submit', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ cardIndex })
+            });
+          }
+        </script>
+      </body>
+    </html>
+  `);
+});
+
+app.post("/client1/submit", (req, res) => {
+  client1.acceptInput(req.body.cardIndex);
+  
+  res.sendStatus(200);
+});
+
+app.get("/client2", (req, res) => {
+  const deck = client2.getDeck();
+  let deckButtons = "";
+  for (let i = 0; i < deck.length; i++) {
+    deckButtons += `<button onclick="submitCard(${i})">${deck[i].cardType} ${deck[i].score}</button>`;
+  }
+  res.send(`
+    <html>
+      <body>
+        <p>${client2.name}'s current deck:</p>
+        ${deckButtons}
+        <script>
+          function submitCard(cardIndex) {
+            fetch('/client2/submit', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ cardIndex })
+            });
+          }
+        </script>
+      </body>
+    </html>
+  `);
+});
+
+app.post("/client2/submit", (req, res) => {
+  client2.acceptInput(req.body.cardIndex);
+  res.sendStatus(200);
+});
+
+app.listen(3001, () => {
   console.log("Example app listening on port 3000!");
 });
