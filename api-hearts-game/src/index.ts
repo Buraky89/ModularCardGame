48123caf2159f02c17010ca1032d679fd1e0283a
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-02-26 21:26:42 +0100

    format

--- api-hearts-game/src/index.ts
+++ api-hearts-game/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
 });
 
 const port = 3001;
