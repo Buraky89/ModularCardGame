4730a64ddccdabc9990d37cdc98483f476b6ccaa
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-03-04 17:58:18 +0100

    introduce RepresentedCard class in api

--- api-hearts-game/src/Common/Card.ts
+++ api-hearts-game/src/Common/Card.ts
@@ -5,6 +5,18 @@ enum CardType {
   CLUBS = "Clubs",
 }
 
+export class RepresentedCard {
+  score?: number;
+  cardType?: CardType;
+  hidden: boolean;
+
+  constructor(score?: number, cardType?: CardType, hidden: boolean = false) {
+    this.score = score;
+    this.cardType = cardType;
+    this.hidden = hidden;
+  }
+}
+
 class Card {
   public score: number;
   public cardType: CardType;
@@ -12,6 +24,15 @@ class Card {
   constructor(score: number, cardType: CardType) {
     this.score = score;
     this.cardType = cardType;
+  }
+
+  toRepresentedCard(isHidden: boolean): RepresentedCard {
+    const representedCard: RepresentedCard = {
+      score: isHidden ? undefined : this.score,
+      cardType: isHidden ? undefined : this.cardType,
+      hidden: isHidden,
+    };
+    return representedCard;
   }
 }
 
--- api-hearts-game/src/Common/Player.ts
+++ api-hearts-game/src/Common/Player.ts
@@ -1,42 +1,57 @@
-import { Card } from "./Card";
+import { response } from "express";
+import { Card, RepresentedCard } from "./Card";
 
 class Player {
   name: string;
   uuid: string;
-  private deck: Card[];
+  private _deck: Card[];
   points: number;
   isTheirTurn: boolean;
+  deck: RepresentedCard[];
 
   constructor(name: string, uuid: string) {
     this.name = name;
     this.uuid = uuid;
-    this.deck = [];
+    this._deck = [];
     this.points = 0;
     this.isTheirTurn = false;
+    this.deck = [];
   }
 
   setCards(cards: Card[]) {
-    this.deck = cards;
+    this._deck = cards;
   }
 
   getDeck(): Card[] {
-    return this.deck;
+    return this._deck;
   }
 
   setIsTheirTurn(isTheirTurn: boolean) {
     this.isTheirTurn = isTheirTurn;
   }
 
+  updateDeck() {
+    console.log("update deck working");
+    const representedDeck: RepresentedCard[] = [];
+    for (const card of this._deck) {
+      const representedCard = card.toRepresentedCard(true);
+      representedDeck.push(representedCard);
+    }
+    console.log("deck", representedDeck);
+    this.deck = representedDeck;
+  }
+
   async playTurn(turnNumber: number, playedDeck: any, selectedIndex: number) {
     // Check if the player has a card in their deck
-    if (this.deck.length === 0) {
+    if (this._deck.length === 0) {
       console.log(`${this.name} has no more cards in their deck.`);
       return null;
     }
 
     console.log(`Which card would you like to play?`, selectedIndex);
-    const card = this.deck[selectedIndex];
-    this.deck.splice(selectedIndex, 1);
+    const card = this._deck[selectedIndex];
+    this._deck.splice(selectedIndex, 1);
+    this.updateDeck();
     this.points += turnNumber * card.score;
     playedDeck.push(card);
     return {
