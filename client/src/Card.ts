772e8df96612d750f219a0b45cb0ffa46a4a9dc6
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-03-04 03:20:31 +0100

    update ApiResponse interface in client

--- client/src/Card.ts
+++ client/src/Card.ts
@@ -1,28 +1,36 @@
 export class Card {
-    score: number;
-    cardType: CardType;
-  
-    constructor(score: number, cardType: CardType) {
-      this.score = score;
-      this.cardType = cardType;
-    }
-  
-    play() {
-      return { score: this.score, cardType: this.cardType };
-    }
+  score: number;
+  cardType: CardType;
+
+  constructor(score: number, cardType: CardType) {
+    this.score = score;
+    this.cardType = cardType;
   }
-  
-  export enum CardType {
-    SPADES = "Spades",
-    HEARTS = "Hearts",
-    DIAMONDS = "Diamonds",
-    CLUBS = "Clubs",
+
+  play() {
+    return { score: this.score, cardType: this.cardType };
   }
-  
+}
+
+export enum CardType {
+  SPADES = "Spades",
+  HEARTS = "Hearts",
+  DIAMONDS = "Diamonds",
+  CLUBS = "Clubs",
+}
 
 export interface ApiResponse {
   deck: Card[];
   playedDeck: {
     deck: Card[];
   };
+  players: Player[];
+}
+
+interface Player {
+  name: string;
+  uuid: string;
+  deck: Card[];
+  points: number;
+  isTheirTurn: boolean;
 }
