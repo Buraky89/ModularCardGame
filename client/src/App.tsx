4df8cc39172ad4172d18c184eb8ee60e731f9fb5
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-03-24 01:36:02 +0100

    modify the app to a dummy state manager app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,147 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import "./App.css";
-import CardsList from "./CardsList";
-import { playerNames } from "./Names";
-import Games from "./Games";
-import { AppStateManager } from "./Common/AppStateManager";
-import { LoginResponse, JoinResponse, ErrorResponse } from "./Common/ResponseTypes";
+import React, { useState, useEffect } from 'react';
+import { GameClient, State } from './Common/StateManager';
 
-function App() {
-  const randomPlayerName = playerNames[Math.floor(Math.random() * playerNames.length)];
-  const [playerName, setPlayerName] = useState(randomPlayerName);
-  const [joined, setJoined] = useState(false);
-  const [token, setToken] = useState<string | null>(null);
-  const [gameUuids, setGameUuids] = useState<string[]>([]);
-  const [appState, setAppState] = useState({ state: 'notLoggedIn', gameUuid: '' });
-  const appStateManager = new AppStateManager();
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch("http://localhost:3001/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username: playerName }),
-      });
-
-      const data: LoginResponse = await response.json();
-
-      if (response.ok) {
-        console.log("Logged in successfully");
-        setToken(data.token);
-        setJoined(true);
-        
-        const loginResponse: LoginResponse = {
-          token: data.token
-        };
-        const messageEvent = new MessageEvent<LoginResponse>("success", {
-          data: loginResponse
-        });
-
-        appStateManager.handleLoginSuccess(messageEvent);
-      } else {
-        console.error(data);
-
-        const errorResponse: ErrorResponse = {
-          message: data.toString()
-        };
-        const errorEvent = new MessageEvent<ErrorResponse>("error", {
-          data: errorResponse
-        });
-
-        appStateManager.handleLoginError(errorEvent);
-      }
-    } catch (err) {
-      console.error(err);
-
-      const errorResponse: ErrorResponse = {
-        message: "Network error"
-      };
-      const errorEvent = new MessageEvent<ErrorResponse>("error", {
-        data: errorResponse
-      });
-
-      appStateManager.handleLoginError(errorEvent);
-    }
-  };
-
-  const handleSelectGame = async (uuid: string) => {
-    try {
-      const response = await fetch("http://localhost:3001/join", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`,
-        },
-        body: JSON.stringify({ token, gameUuid: uuid }),
-      });
-
-      const data: JoinResponse = await response.json();
-
-      if (response.ok) {
-        console.log(data.message);
-        //setUuid(data.uuid);
-        //setGameUuid(uuid);
-        appStateManager.selectGame(uuid);
-      } else {
-        console.error(data.message);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    const getGames = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/getGames");
-        const data = await response.json();
-
-        if (response.ok && data.length > 0) {
-          setGameUuids(data);
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getGames();
-  }, []);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setAppState(appStateManager.getState());
-    }, 500);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <div>
-        <label>
-          Player Name:
-          <input
-            type="text"
-            value={playerName}
-            onChange={(e) => setPlayerName(e.target.value)}
-          />
-        </label>
-        <button onClick={handleLogin}>Login</button>
-      </div>
-      {appState.state === 'inGame' && joined && token && <CardsList uuid={uuidv4()} token={token} gameUuid={appState.gameUuid} />}
-      {appState.state === 'connectingToGames' && <div>Connecting to games...</div>}
-      {appState.state === 'notLoggedIn' && <div>Not logged in</div>}
-      {appState.state === 'loginError' && <div>Login error</div>}
-      {appState.state === 'connectionLostWaiting' && <div>Connection lost. Waiting...</div>}
-      {appState.state === 'notLoggedIn' || appState.state === 'loginError' || appState.state === 'connectingToGames' ?
-        <Games uuids={gameUuids} onSelect={handleSelectGame} /> : null}
-    </div>
-  );
+interface AppState {
+  state: string;
+  gameUuids: string[];
+  gameUuid: string;
+  userUuid: string;
+  jwtToken: string;
 }
 
-export default App;
+const TestTest: React.FC = () => {
+  const [client] = useState(new GameClient());
+  const [loginName, setLoginName] = useState('');
+  const [appState, setAppState] = useState<AppState>({
+    state: 'NotLoggedIn',
+    gameUuids: [],
+    gameUuid: '',
+    userUuid: '',
+    jwtToken: '',
+  });
+
+  useEffect(() => {
+
+    client.socket.on("any", (message: any) => {
+      const newState: AppState = {
+        state: State[client.stateManager.state],
+        gameUuids: client.stateManager.gameUuids,
+        gameUuid: client.stateManager.gameUuid,
+        userUuid: client.stateManager.userUuid,
+        jwtToken: client.stateManager.jwtToken,
+      };
+      setAppState(newState);
+    });
+  }, [client]);
+
+  const handleLogin = () => {
+    client.login(loginName);
+  };
+
+  return (
+    <div>
+      <h1>Game Client</h1>
+      <p>
+        State: {appState.state}
+        <br />
+        JWT Token: {appState.jwtToken}
+        <br />
+        Game UUIDs: {appState.gameUuids.join(', ')}
+        <br />
+        Game UUID: {appState.gameUuid}
+        <br />
+        User UUID: {appState.userUuid}
+      </p>
+      <input
+        type="text"
+        value={loginName}
+        onChange={(e) => setLoginName(e.target.value)}
+        placeholder="Enter login name"
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default TestTest;
---
+++ client/src/Common/StateManager.ts
@@ -0,0 +1,103 @@
+import { EventEmitter } from "events";
+import { SocketServerMock } from "socket.io-mock-ts";
+
+export enum State {
+  NotLoggedIn,
+  LoginError,
+  GameListLoading,
+  GameListLoaded,
+  WaitingForGame,
+  SubscribedToGame,
+  ConnectionLostWaiting,
+}
+
+export class StateManager {
+  state: State;
+  gameUuids: string[];
+  gameUuid: string;
+  userUuid: string;
+  jwtToken: string;
+
+  constructor() {
+    this.state = State.NotLoggedIn;
+    this.gameUuids = [];
+    this.gameUuid = "";
+    this.userUuid = "";
+    this.jwtToken = "";
+  }
+
+  setState(newState: State) {
+    this.state = newState;
+  }
+
+  setJwtToken(jwtToken: string) {
+    this.jwtToken = jwtToken;
+  }
+
+  setGameUuids(gameUuids: string[]) {
+    this.gameUuids = gameUuids;
+  }
+
+  setGameUuid(gameUuid: string) {
+    this.gameUuid = gameUuid;
+  }
+}
+
+export class GameClient {
+  stateManager: StateManager;
+  socket: SocketServerMock;
+
+  constructor() {
+    this.stateManager = new StateManager();
+    this.socket = new SocketServerMock();
+    this.init();
+  }
+
+  init() {
+    this.socket.on("loginError", () => {
+      this.stateManager.setState(State.LoginError);
+    });
+
+    this.socket.on("loginSuccess", (payload: { jwtToken: string }) => {
+      this.stateManager.setState(State.GameListLoading);
+      this.stateManager.setJwtToken(payload.jwtToken);
+    });
+
+    this.socket.on("gameListCame", (payload: { gameUuids: string[] }) => {
+      this.stateManager.setState(State.GameListLoaded);
+      this.stateManager.setGameUuids(payload.gameUuids);
+    });
+
+    this.socket.on(
+      "userSelectedTheGameUuid",
+      (payload: { gameUuid: string }) => {
+        this.stateManager.setState(State.WaitingForGame);
+        this.stateManager.setGameUuid(payload.gameUuid);
+      }
+    );
+
+    this.socket.on("subscribedToGame", () => {
+      this.stateManager.setState(State.SubscribedToGame);
+    });
+
+    this.socket.on("connectionLost", () => {
+      console.log("en azından connectionLost çalıştı");
+      this.stateManager.setState(State.ConnectionLostWaiting);
+    });
+  }
+
+  login(loginName: string) {
+    this.socket.emit("login", { loginName });
+
+    // Mock loginSuccess or loginError event
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        this.socket.clientMock.emit("loginSuccess", {
+          jwtToken: "mockJWTToken",
+        });
+      } else {
+        this.socket.clientMock.emit("loginError");
+      }
+    }, 1000);
+  }
+}
