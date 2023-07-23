e9825ef3135ee07e2aca9647f3023998c021564d
buraky89 <2487413+Buraky89@users.noreply.github.com>
2023-07-23 20:10:40 +0200

    refactor event versioning, make some events omit version number

--- api-hearts-game/src/Common/EventFactory.ts
+++ api-hearts-game/src/Common/EventFactory.ts
@@ -5,132 +5,99 @@ import { Player } from './Player';
 import GameState from './Enums';
 
 export class EventFactory {
-    static version: number = 1;
-    static versionForGeneralEvents: number = 1;
+    static version: number = 0;
+    static versionForGeneralEvents: number = 0;
 
     static incrementVersion() {
-        this.version++;
+        return this.version++;
     }
 
     static incrementVersionForGeneralEvents() {
-        this.versionForGeneralEvents++;
+        return this.versionForGeneralEvents++;
     }
 
     static newPlayerWantsToJoin(date: Date, ip: string, uuid: string, playerName: string): Event {
-        this.incrementVersionForGeneralEvents();
         const payload = PayloadFactory.newPlayerWantsToJoin(date, ip, uuid, playerName);
-        return new Event(Events.NewPlayerWantsToJoin, payload, this.versionForGeneralEvents);
+        return new Event(Events.NewPlayerWantsToJoin, payload, this.incrementVersion());
     }
 
     static generalUpdateMessage(gameUuidList: string[]): Event {
-        this.incrementVersionForGeneralEvents();
-
         const payload = PayloadFactory.generalUpdateMessage(gameUuidList);
-        return new Event(Events.GeneralUpdateMessage, payload, this.versionForGeneralEvents);
+        return new Event(Events.GeneralUpdateMessage, payload, this.incrementVersionForGeneralEvents());
     }
 
     static playerPlayed(uuid: string, selectedIndex: number): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.playerPlayed(uuid, selectedIndex);
-        return new Event(Events.PlayerPlayed, payload, this.version);
+        return new Event(Events.PlayerPlayed, payload, this.incrementVersion());
     }
 
     static newPlayerApprovedToJoin(uuid: string, approvedAt: Date): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.newPlayerApprovedToJoin(uuid, approvedAt);
-        return new Event(Events.NewPlayerApprovedToJoin, payload, this.version);
+        return new Event(Events.NewPlayerApprovedToJoin, payload, this.incrementVersion());
     }
 
     static newViewerApprovedToSubscribe(uuid: string, approvedAt: Date): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.newViewerApprovedToSubscribe(uuid, approvedAt);
-        return new Event(Events.NewViewerApprovedToSubscribe, payload, this.version);
+        return new Event(Events.NewViewerApprovedToSubscribe, payload, this.incrementVersion());
     }
 
     static playerAttemptsToPlay(uuid: string, selectedIndex: number): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.playerAttemptsToPlay(uuid, selectedIndex);
-        return new Event(Events.PlayerAttemptsToPlay, payload, this.version);
+        return new Event(Events.PlayerAttemptsToPlay, payload, this.incrementVersion());
     }
 
     static cardsAreDistributed(): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.cardsAreDistributed();
-        return new Event(Events.CardsAreDistributed, payload, this.version);
+        return new Event(Events.CardsAreDistributed, payload, this.incrementVersion());
     }
 
     static gameMessageToPlayer(uuid: string, playerUuid: string, message: string): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.gameMessageToPlayer(uuid, playerUuid, message);
-        return new Event(Events.GameMessageToPlayer, payload, this.version);
+        return new Event(Events.GameMessageToPlayer, payload, 0); // TODO: invent maybe a new series of version for this, instead of 0.
     }
 
     static gameStartRequested(uuid: string): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.gameStartRequested(uuid);
-        return new Event(Events.GameStartRequested, payload, this.version);
+        return new Event(Events.GameStartRequested, payload, this.incrementVersion());
     }
 
     static gameStartApproved(uuid: string): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.gameStartApproved(uuid);
-        return new Event(Events.GameStartApproved, payload, this.version);
+        return new Event(Events.GameStartApproved, payload, this.incrementVersion());
     }
 
     static gameEnded(winner: Player, players: Player[]): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.gameEnded(winner, players);
-        return new Event(Events.GameEnded, payload, this.version);
+        return new Event(Events.GameEnded, payload, this.incrementVersion());
     }
 
     static newViewerWantsToSubscribeGeneral(date: Date, ip: string, uuid: string, username: string): Event {
-        this.incrementVersionForGeneralEvents();
-
         const payload = PayloadFactory.newPlayerWantsToJoin(date, ip, uuid, username);
-        return new Event(Events.NewViewerWantsToSubscribeGeneral, payload, this.version);
+        return new Event(Events.NewViewerWantsToSubscribeGeneral, payload, this.incrementVersionForGeneralEvents());
     }
 
     static newViewerWantsToSubscribe(date: Date, ip: string, uuid: string, username: string): Event {
-        this.incrementVersion();
-
         const payload = PayloadFactory.newPlayerWantsToJoin(date, ip, uuid, username);
-        return new Event(Events.NewViewerWantsToSubscribe, payload, this.version);
+        return new Event(Events.NewViewerWantsToSubscribe, payload, this.incrementVersion());
     }
 
     static generalUpdateMessageExchange(gameUuidList: string[], viewerUuid: string): Event {
-        this.incrementVersionForGeneralEvents();
-
         const payload = PayloadFactory.generalUpdateMessageExchange(gameUuidList, viewerUuid);
-        return new Event(Events.GeneralUpdateMessageExchange, payload, this.versionForGeneralEvents);
+        return new Event(Events.GeneralUpdateMessageExchange, payload, this.incrementVersionForGeneralEvents());
     }
 
     static cardsAreReadyToBeDistributed(): Event {
-        this.incrementVersion();
-
-        return new Event(Events.CardsAreReadyToBeDistributed, PayloadFactory.cardsAreDistributed(), this.version);
+        return new Event(Events.CardsAreReadyToBeDistributed, PayloadFactory.cardsAreDistributed(), this.incrementVersion());
     }
 
     static gameUpdated(gameUuid: string, data: GameState): Event {
-        this.incrementVersion();
-
-        const payload = { version: this.version, gameUuid, data };
-        return new Event(Events.GameUpdated, payload, this.version);
+        const payload = { gameUuid, data };
+        return new Event(Events.GameUpdated, payload, 0); // TODO: invent maybe a new series of version for this, instead of 0.
     }
 
     static gameRestarted(): Event {
-        this.incrementVersion();
-
-        const payload = { version: this.version };
-        return new Event(Events.GameRestarted, payload, this.version);
+        const payload = {};
+        return new Event(Events.GameRestarted, payload, this.incrementVersion());
     }
 
 }
